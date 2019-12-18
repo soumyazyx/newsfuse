e6a18d153d32626c1c337caa00cf38459201711b
Soumya Ranjan Das <soumyazyx@gmail.com>
2019-12-19 01:08:57 +0530

    Added home app

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
+import home.views
 
 # To add a new path, first import the app:
 # import blog
@@ -15,7 +16,8 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
+    path("", home.views.index, name="home-index"),
+    path("hello/", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
