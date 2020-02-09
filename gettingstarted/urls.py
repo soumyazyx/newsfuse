77c6e8d8618a1a6077afd300a542caa5a157da8f
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-02-09 22:05:00 +0530

    Removed hello app

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,23 +1,9 @@
 from django.urls import path, include
-
 from django.contrib import admin
-
-admin.autodiscover()
-
-import hello.views
 import home.views
-
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+admin.autodiscover()
 
 urlpatterns = [
     path("", home.views.index, name="home-index"),
-    path("hello/", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
