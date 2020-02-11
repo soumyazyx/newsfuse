b2ac4a5808d21aed97cce74345cfb49499478db8
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-02-12 01:35:08 +0530

    added signup model

--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Signup
+
 import feedparser
 
 
 def index(request):
+
+    email_list = Signup.objects.all()
+    if request.method == 'POST':
+        email = request.POST['email']
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+
     feed = feedparser.parse('https://www.nasa.gov/rss/dyn/breaking_news.rss')
     context = {
-        'feed': feed
+        'feed': feed,
+        'email_list': email_list
     }
     return render(request, "index.html", context)
