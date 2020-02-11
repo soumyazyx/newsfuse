07a971d0d6a4859cb3419dabf71f08ef923cd41d
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-02-12 02:23:50 +0530

    Added elephant SQL connection

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cxneygrs",
+        "USER": "cxneygrs",
+        "PASSWORD": "17TtwsrLMaackDRZgRWxowtK_mdU2f9R",
+        "HOST": "rajje.db.elephantsql.com",
+        "PORT": "5432"
     }
 }
 
